79d5e4d619d39961d48e151106a4537c55ddae03
sukhanovai <aleksandr.i.sukhanov@gmail.com>
2026-01-15 23:04:54 +0700

    Добавил раздел «Бэкапы почты» в меню расширений и отдельный экран для управления паттернами бэкапов почты, включая привязку к настройкам паттернов.
    Расширил обработчики паттернов бэкапов, чтобы поддерживать добавление, просмотр и редактирование паттернов категории mail.
    
    Почтовый мониторинг теперь читает паттерны бэкапов почты из конфигурации и применяет их при разборе писем с запасным шаблоном.

--- extensions/base.py
+++ extensions/base.py
@@ -1,11 +1,11 @@
 """
 /extensions/base.py
-Server Monitoring System v7.1.4
+Server Monitoring System v7.1.5
 Copyright (c) 2025 Aleksandr Sukhanov
 License: MIT
 Extensions interface
 Система мониторинга серверов
-Версия: 7.1.4
+Версия: 7.1.5
 Автор: Александр Суханов (c)
 Лицензия: MIT
 Интерфейс расширений
