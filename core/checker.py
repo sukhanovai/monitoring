8ac1189e2f766a60dc5bcdaab4adb3af1c68c572
sukhanovai <aleksandr.i.sukhanov@gmail.com>
2026-01-26 01:44:05 +0700

    Добавил настройку постфикса артикула в меню правил обработки, подсказки и обработку ввода в мастере настройки.
    Постфикс учитывается при формировании выходных артикулов в файлах остатков

--- core/checker.py
+++ core/checker.py
@@ -1,11 +1,11 @@
 """
 /core/checker.py
-Server Monitoring System v8.2.32
+Server Monitoring System v8.2.33
 Copyright (c) 2025 Aleksandr Sukhanov
 License: MIT
 Server Checker Module
 Система мониторинга серверов
-Версия: 8.2.32
+Версия: 8.2.33
 Автор: Александр Суханов (c)
 Лицензия: MIT
 Модуль проверки серверов
