8cb1f55ff2c0cb605f50db0a4ebaa58f71069b55
Alexandr <aleksandr.i.sukhanov@gmail.com>
2026-02-23 02:35:40 +0700

    Добавить совместимость со старой ссылкой в MainViewModel

--- android-client/app/src/main/java/ru/monitoring/mobile/storage/AppPreferences.kt
+++ android-client/app/src/main/java/ru/monitoring/mobile/storage/AppPreferences.kt
@@ -11,7 +11,15 @@ class AppPreferences(context: Context) {
             prefs.edit().putString(KEY_API_TOKEN, value).apply()
         }
 
+    var apiBaseUrl: String
+        get() = prefs.getString(KEY_API_BASE_URL, DEFAULT_API_BASE_URL) ?: DEFAULT_API_BASE_URL
+        set(value) {
+            prefs.edit().putString(KEY_API_BASE_URL, value).apply()
+        }
+
     companion object {
         private const val KEY_API_TOKEN = "api_token"
+        private const val KEY_API_BASE_URL = "api_base_url"
+        private const val DEFAULT_API_BASE_URL = "https://api.202020.ru:8443/"
     }
 }
