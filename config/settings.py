6a1d84983da65efc51fd1924df9e83a270487b3e
Alexandr <aleksandr.i.sukhanov@gmail.com>
2026-03-15 01:07:19 +0700

    Убрана жесткая Kotlin toolchain настройка и версии обновлены до 8.32.7

--- config/settings.py
+++ config/settings.py
@@ -1,11 +1,11 @@
 """
 /config/settings.py
-Server Monitoring System v8.31.0
+Server Monitoring System v8.32.7
 Copyright (c) 2025 Aleksandr Sukhanov
 License: MIT
 Application settings - default values
 Система мониторинга серверов
-Версия: 8.31.0
+Версия: 8.32.7
 Автор: Александр Суханов (c)
 Лицензия: MIT
 Настройки приложения - значения по умолчанию
@@ -22,10 +22,10 @@ from lib.utils import is_proxmox_server
 DEBUG_MODE = False
 
 # Версия приложения
-APP_VERSION = "8.31.0"
-ANDROID_APP_VERSION = "8.31.0"
-ANDROID_MIN_SUPPORTED_VERSION = "8.31.0"
-ANDROID_LATEST_VERSION = "8.31.0"
+APP_VERSION = "8.32.7"
+ANDROID_APP_VERSION = "8.32.7"
+ANDROID_MIN_SUPPORTED_VERSION = "8.32.7"
+ANDROID_LATEST_VERSION = "8.32.7"
 ANDROID_APK_DOWNLOAD_URL = os.environ.get(
     "ANDROID_APK_DOWNLOAD_URL",
     "https://github.com/<org>/<repo>/releases/latest/download/monitoring-android.apk",
