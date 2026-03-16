b18fa64c04d4f9bccd2f614b21a99d07bbbeb4d1
Alexandr <aleksandr.i.sukhanov@gmail.com>
2026-03-16 20:59:31 +0700

    Синхронизировать версии проекта во всех файлах

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """
 /main.py
-Server Monitoring System v8.32.36
+Server Monitoring System v8.32.38
 Copyright (c) 2025 Aleksandr Sukhanov
 License: MIT
 Main launch module
 Система мониторинга серверов
-Версия: 8.32.36
+Версия: 8.32.38
 Автор: Александр Суханов (c)
 Лицензия: MIT
 Основной модуль запуска
