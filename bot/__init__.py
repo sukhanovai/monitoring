4542b1b20458f48d7d7fb740c37e868f4e33a950
Alexandr <aleksandr.i.sukhanov@gmail.com>
2026-03-19 00:22:29 +0700

    Исправить экран расширений Android и поднять версию

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,11 +1,11 @@
 """
 /bot/__init__.py
-Server Monitoring System v8.33.9
+Server Monitoring System v8.33.10
 Copyright (c) 2025 Aleksandr Sukhanov
 License: MIT
 Telegram bot module
 Система мониторинга серверов
-Версия: 8.33.9
+Версия: 8.33.10
 Автор: Александр Суханов (c)
 Лицензия: MIT
 Модуль Telegram-бота
