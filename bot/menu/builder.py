c118a570fd77eda92416ccabb6d5a7091843ef34
Alexandr <aleksandr.i.sukhanov@gmail.com>
2026-03-19 15:52:09 +0700

    Исправлен обработчик действий настроек расширений

--- bot/menu/builder.py
+++ bot/menu/builder.py
@@ -1,11 +1,11 @@
 """
 /bot/menu/builder.py
-Server Monitoring System v8.33.19
+Server Monitoring System v8.33.20
 Copyright (c) 2025 Aleksandr Sukhanov
 License: MIT
 The place where keyboards are made.
 Система мониторинга серверов
-Версия: 8.33.19
+Версия: 8.33.20
 Автор: Александр Суханов (c)
 Лицензия: MIT
 Место, где строятся клавиатуры
