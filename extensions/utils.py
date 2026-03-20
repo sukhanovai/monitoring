50196eb37d4d6e5955cf24ea7b477dec45488143
Alexandr <aleksandr.i.sukhanov@gmail.com>
2026-03-21 01:55:24 +0700

    Исправлен список Proxmox-хостов в mobile settings

--- extensions/utils.py
+++ extensions/utils.py
@@ -1,11 +1,11 @@
 """
 /extensions/utils.py
-Server Monitoring System v8.33.37
+Server Monitoring System v8.33.38
 Copyright (c) 2025 Aleksandr Sukhanov
 License: MIT
 Temporary shim module for compatibility.
 Система мониторинга серверов
-Версия: 8.33.37
+Версия: 8.33.38
 Автор: Александр Суханов (c)
 Лицензия: MIT
 Временный shim-модуль для совместимости.
--- lib/helpers.py
+++ lib/helpers.py
@@ -1,11 +1,11 @@
 """
 /lib/helpers.py
-Server Monitoring System v8.33.37
+Server Monitoring System v8.33.38
 Copyright (c) 2025 Aleksandr Sukhanov
 License: MIT
 Auxiliary utilities
 Система мониторинга серверов
-Версия: 8.33.37
+Версия: 8.33.38
 Автор: Александр Суханов (c)
 Лицензия: MIT
 Вспомогательные утилиты
