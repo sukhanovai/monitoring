1e727ad71bb5d69c639a7aca3aa1bb3904eab7ee
Alexandr <aleksandr.i.sukhanov@gmail.com>
2026-03-26 15:47:44 +0700

    Исправлен post-pull Android скрипт и выполнен бамп версии до 8.33.62

--- core/task_router.py
+++ core/task_router.py
@@ -1,11 +1,11 @@
 """
 /core/task_router.py
-Server Monitoring System v8.33.61
+Server Monitoring System v8.33.62
 Copyright (c) 2025 Aleksandr Sukhanov
 License: MIT
 Task router helpers
 Система мониторинга серверов
-Версия: 8.33.61
+Версия: 8.33.62
 Автор: Александр Суханов (c)
 Лицензия: MIT
 Хелперы маршрутизации задач
