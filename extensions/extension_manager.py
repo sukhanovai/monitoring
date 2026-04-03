687f829a1ae6018434286d5f8d46a90eaeaf8fd3
Alexandr <aleksandr.i.sukhanov@gmail.com>
2026-04-03 20:43:39 +0700

    android: обновить UI точечной проверки и версию 8.41.5

--- extensions/extension_manager.py
+++ extensions/extension_manager.py
@@ -1,11 +1,11 @@
 """
 /extensions/extension_manager.py
-Server Monitoring System v8.41.4
+Server Monitoring System v8.41.5
 Copyright (c) 2025 Aleksandr Sukhanov
 License: MIT
 Extension Manager for Monitoring
 Система мониторинга серверов
-Версия: 8.41.4
+Версия: 8.41.5
 Автор: Александр Суханов (c)
 Лицензия: MIT
 Менеджер расширений для мониторинга
