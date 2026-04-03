4488a6e314ca32a386016f1710eb8944c128ca1d
Alexandr <aleksandr.i.sukhanov@gmail.com>
2026-04-03 22:34:57 +0700

    Android: Proxmox плашки и бамп версии 8.41.11

--- modules/targeted_resources.py
+++ modules/targeted_resources.py
@@ -1,11 +1,11 @@
 """
 /modules/targeted_resources.py
-Server Monitoring System v8.41.10
+Server Monitoring System v8.41.11
 Copyright (c) 2025 Aleksandr Sukhanov
 License: MIT
 A placeholder for address verification of resources.
 Система мониторинга серверов
-Версия: 8.41.10
+Версия: 8.41.11
 Автор: Александр Суханов (c)
 Лицензия: MIT
 Заглушка для адресных проверок ресурсов.
