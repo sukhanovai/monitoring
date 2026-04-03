f33a5e474ff67ef2981d99fb91bce04db850f46f
Alexandr <aleksandr.i.sukhanov@gmail.com>
2026-04-03 21:34:24 +0700

    Исправить окно точечной проверки и обновить версию до 8.41.7

--- extensions/base.py
+++ extensions/base.py
@@ -1,11 +1,11 @@
 """
 /extensions/base.py
-Server Monitoring System v8.41.6
+Server Monitoring System v8.41.7
 Copyright (c) 2025 Aleksandr Sukhanov
 License: MIT
 Extensions interface
 Система мониторинга серверов
-Версия: 8.41.6
+Версия: 8.41.7
 Автор: Александр Суханов (c)
 Лицензия: MIT
 Интерфейс расширений
