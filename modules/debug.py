dcfacb2b33a349682e88d0645c76bbef2b904f16
Alexandr <aleksandr.i.sukhanov@gmail.com>
2026-04-06 16:26:09 +0700

    Доработать управление Proxmox в Android и бамп версии

--- modules/debug.py
+++ modules/debug.py
@@ -1,11 +1,11 @@
 """
 /app/modules/debug.py
-Server Monitoring System v8.41.35
+Server Monitoring System v8.41.36
 Copyright (c) 2025 Aleksandr Sukhanov
 License: MIT
 Debugging and diagnostics module
 Система мониторинга серверов
-Версия: 8.41.35
+Версия: 8.41.36
 Автор: Александр Суханов (c)
 Лицензия: MIT
 Модуль отладки и диагностики
