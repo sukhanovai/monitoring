84d72affeee093b57f708fc63e089d2af9a625a0
Alexandr <aleksandr.i.sukhanov@gmail.com>
2026-04-08 18:26:15 +0700

    Добавить управление хостами Proxmox в меню бэкапов

--- core/checker.py
+++ core/checker.py
@@ -1,11 +1,11 @@
 """
 /core/checker.py
-Server Monitoring System v8.42.5
+Server Monitoring System v8.43.0
 Copyright (c) 2025 Aleksandr Sukhanov
 License: MIT
 Server Checker Module
 Система мониторинга серверов
-Версия: 8.42.5
+Версия: 8.43.0
 Автор: Александр Суханов (c)
 Лицензия: MIT
 Модуль проверки серверов
