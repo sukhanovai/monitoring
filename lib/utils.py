8267ad8c40d16d7dec06a652b26cde2ca8bbbeb3
Alexandr <aleksandr.i.sukhanov@gmail.com>
2026-04-08 19:47:50 +0700

    Андроид: статусы мониторинга БД в оперативном центре

--- lib/utils.py
+++ lib/utils.py
@@ -1,11 +1,11 @@
 """
 /lib/utils.py
-Server Monitoring System v8.45.1
+Server Monitoring System v8.45.2
 Copyright (c) 2025 Aleksandr Sukhanov
 License: MIT
 Utility functions
 Система мониторинга серверов
-Версия: 8.45.1
+Версия: 8.45.2
 Автор: Александр Суханов (c)
 Лицензия: MIT
 Вспомогательные функции
