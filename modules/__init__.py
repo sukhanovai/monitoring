f6dc9bdccf8c89b4cee1ffb415750c74bceed6e5
Alexandr <aleksandr.i.sukhanov@gmail.com>
2026-04-10 02:02:25 +0700

    android: доработка окна бэкапов почты и бамп 8.50.24

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,11 +1,11 @@
 """
 /app/modules/__init__.py
-Server Monitoring System v8.50.23
+Server Monitoring System v8.50.24
 Copyright (c) 2025 Aleksandr Sukhanov
 License: MIT
 Monitoring system modules
 Система мониторинга серверов
-Версия: 8.50.23
+Версия: 8.50.24
 Автор: Александр Суханов (c)
 Лицензия: MIT
 Модули системы мониторинга
