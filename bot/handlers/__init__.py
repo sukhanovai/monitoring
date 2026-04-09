207deffe698aaa3cf60eec0cade9bd2d907d0f36
Alexandr <aleksandr.i.sukhanov@gmail.com>
2026-04-09 13:41:12 +0700

    Добавить крестик закрытия в диалоге БД

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,11 +1,11 @@
 """
 /bot/handlers/__init__.py
-Server Monitoring System v8.48.27
+Server Monitoring System v8.48.28
 Copyright (c) 2025 Aleksandr Sukhanov
 License: MIT
 Handlers package exports
 Система мониторинга серверов
-Версия: 8.48.27
+Версия: 8.48.28
 Автор: Александр Суханов (c)
 Лицензия: MIT
 Экспорт вспомогательных функций для регистрации обработчиков
