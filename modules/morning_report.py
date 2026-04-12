bec0d098158a8a39d2d8898b6c290220eb852d54
Alexandr <aleksandr.i.sukhanov@gmail.com>
2026-04-13 01:05:23 +0700

    Перенесена шестерёнка ресурсов и поднята версия до 8.50.63

--- modules/morning_report.py
+++ modules/morning_report.py
@@ -1,11 +1,11 @@
 """
 /app/modules/morning_report.py
-Server Monitoring System v8.50.62
+Server Monitoring System v8.50.63
 Copyright (c) 2025 Aleksandr Sukhanov
 License: MIT
 Morning Report Module
 Система мониторинга серверов
-Версия: 8.50.62
+Версия: 8.50.63
 Автор: Александр Суханов (c)
 Лицензия: MIT
 Модуль утреннего отчета
