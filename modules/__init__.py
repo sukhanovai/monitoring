064276f1322f73a6fea1dd17d40bd6ac31b91e45
Alexandr <aleksandr.i.sukhanov@gmail.com>
2026-04-16 00:09:29 +0700

    Android: подтверждение удаления сервера

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,11 +1,11 @@
 """
 /app/modules/__init__.py
-Server Monitoring System v8.50.130
+Server Monitoring System v8.50.131
 Copyright (c) 2025 Aleksandr Sukhanov
 License: MIT
 Monitoring system modules
 Система мониторинга серверов
-Версия: 8.50.130
+Версия: 8.50.131
 Автор: Александр Суханов (c)
 Лицензия: MIT
 Модули системы мониторинга
