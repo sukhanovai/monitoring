a43db6e1b94ae89caab0a502b9b77d4eb6b0a05a
Alexandr <aleksandr.i.sukhanov@gmail.com>
2026-04-15 21:31:00 +0700

    Android: перестановка плашек и бамп версии 8.50.122

--- lib/utils.py
+++ lib/utils.py
@@ -1,11 +1,11 @@
 """
 /lib/utils.py
-Server Monitoring System v8.50.121
+Server Monitoring System v8.50.122
 Copyright (c) 2025 Aleksandr Sukhanov
 License: MIT
 Utility functions
 Система мониторинга серверов
-Версия: 8.50.121
+Версия: 8.50.122
 Автор: Александр Суханов (c)
 Лицензия: MIT
 Вспомогательные функции
