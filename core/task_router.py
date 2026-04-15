c51a27e5367dbdef7d2c80c4499847acbc5a92c0
Alexandr <aleksandr.i.sukhanov@gmail.com>
2026-04-16 01:40:06 +0700

    Исправлен тап по шестеренке ресурсов

--- core/task_router.py
+++ core/task_router.py
@@ -1,11 +1,11 @@
 """
 /core/task_router.py
-Server Monitoring System v8.50.132
+Server Monitoring System v8.50.133
 Copyright (c) 2025 Aleksandr Sukhanov
 License: MIT
 Task router helpers
 Система мониторинга серверов
-Версия: 8.50.132
+Версия: 8.50.133
 Автор: Александр Суханов (c)
 Лицензия: MIT
 Хелперы маршрутизации задач
--- modules/morning_report.py
+++ modules/morning_report.py
@@ -1,11 +1,11 @@
 """
 /app/modules/morning_report.py
-Server Monitoring System v8.50.132
+Server Monitoring System v8.50.133
 Copyright (c) 2025 Aleksandr Sukhanov
 License: MIT
 Morning Report Module
 Система мониторинга серверов
-Версия: 8.50.132
+Версия: 8.50.133
 Автор: Александр Суханов (c)
 Лицензия: MIT
 Модуль утреннего отчета
