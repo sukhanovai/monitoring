12e66fc5eb4210cd21c7e60f13d71ca31934a04e
Alexandr <aleksandr.i.sukhanov@gmail.com>
2026-04-19 00:56:39 +0700

    Исправить разделение плашек ZFS в Android

--- lib/alerts.py
+++ lib/alerts.py
@@ -1,11 +1,11 @@
 """
 /lib/alerts.py
-Server Monitoring System v8.55.17
+Server Monitoring System v8.55.18
 Copyright (c) 2025 Aleksandr Sukhanov
 License: MIT
 Unified alert system
 Система мониторинга серверов
-Версия: 8.55.17
+Версия: 8.55.18
 Автор: Александр Суханов (c)
 Лицензия: MIT
 Единая система оповещений
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """
 /main.py
-Server Monitoring System v8.55.17
+Server Monitoring System v8.55.18
 Copyright (c) 2025 Aleksandr Sukhanov
 License: MIT
 Main launch module
 Система мониторинга серверов
-Версия: 8.55.17
+Версия: 8.55.18
 Автор: Александр Суханов (c)
 Лицензия: MIT
 Основной модуль запуска
