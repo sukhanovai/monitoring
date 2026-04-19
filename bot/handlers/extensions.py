62b77eb60af1c92ff4807828202e037f978fe5f4
Alexandr <aleksandr.i.sukhanov@gmail.com>
2026-04-19 19:40:14 +0700

    Android: шестерёнка и форма добавления хоста ZFS

--- bot/handlers/extensions.py
+++ bot/handlers/extensions.py
@@ -1,11 +1,11 @@
 """
 /bot/handlers/extensions.py
-Server Monitoring System v8.55.39
+Server Monitoring System v8.55.40
 Copyright (c) 2025 Aleksandr Sukhanov
 License: MIT
 UI handlers for managing extensions
 Система мониторинга серверов
-Версия: 8.55.39
+Версия: 8.55.40
 Автор: Александр Суханов (c)
 Лицензия: MIT
 UI-обработчики управления расширениями
