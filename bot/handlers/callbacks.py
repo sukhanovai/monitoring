a5e44443e5b0c0f7b95f700e90a4e0b94da3190e
Alexandr <aleksandr.i.sukhanov@gmail.com>
2026-04-20 01:42:24 +0700

    Android: исправлен + в ZFS месте, бамп 8.55.51

--- bot/handlers/callbacks.py
+++ bot/handlers/callbacks.py
@@ -1,11 +1,11 @@
 """
 /bot/handlers/callbacks.py
-Server Monitoring System v8.55.50
+Server Monitoring System v8.55.51
 Copyright (c) 2025 Aleksandr Sukhanov
 License: MIT
 A single router for callbacks.
 Система мониторинга серверов
-Версия: 8.55.50
+Версия: 8.55.51
 Автор: Александр Суханов (c)
 Лицензия: MIT
 Единый router callback’ов.
