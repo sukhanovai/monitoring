d567b0abcb9f370e0b5bbc7597998d3e98bb4f93
Alexandr <aleksandr.i.sukhanov@gmail.com>
2026-05-01 02:13:08 +0700

    Бамп версии до 8.56.67

--- config/db_settings.py
+++ config/db_settings.py
@@ -1,11 +1,11 @@
 """
 /config/db_settings.py
-Server Monitoring System v8.56.65
+Server Monitoring System v8.56.67
 Copyright (c) 2025 Aleksandr Sukhanov
 License: MIT
 Database-backed settings loader
 Система мониторинга серверов
-Версия: 8.56.65
+Версия: 8.56.67
 Автор: Александр Суханов (c)
 Лицензия: MIT
 Загрузчик настроек из базы данных
--- lib/__init__.py
+++ lib/__init__.py
@@ -1,11 +1,11 @@
 """
 /lib/__init__.py
-Server Monitoring System v8.56.65
+Server Monitoring System v8.56.67
 Copyright (c) 2025 Aleksandr Sukhanov
 License: MIT
 Utility library package
 Система мониторинга серверов
-Версия: 8.56.65
+Версия: 8.56.67
 Автор: Александр Суханов (c)
 Лицензия: MIT
 Пакет библиотеки утилит
