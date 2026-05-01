d4d3468a9bfb38a59c3f4d4c393091d816927508
Alexandr <aleksandr.i.sukhanov@gmail.com>
2026-05-02 01:44:09 +0700

    Бамп версии до 8.56.97

--- config/debug.py
+++ config/debug.py
@@ -1,11 +1,11 @@
 """
 /config/debug.py
-Server Monitoring System v8.56.96
+Server Monitoring System v8.56.97
 Copyright (c) 2025 Aleksandr Sukhanov
 License: MIT
 Monitoring system debug configuration
 Система мониторинга серверов
-Версия: 8.56.96
+Версия: 8.56.97
 Автор: Александр Суханов (c)
 Лицензия: MIT
 Конфигурация отладки системы мониторинга
