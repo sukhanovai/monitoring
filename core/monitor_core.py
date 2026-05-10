65d6a28d16c260bdafa730d46e9cc00f461a9080
Alexandr <aleksandr.i.sukhanov@gmail.com>
2026-05-10 22:59:34 +0700

    Синхронизировать упоминания версии 8.58.23

--- core/monitor_core.py
+++ core/monitor_core.py
@@ -1,11 +1,11 @@
 """
 /core/monitor_core.py
-Server Monitoring System v8.58.22
+Server Monitoring System v8.58.23
 Copyright (c) 2025 Aleksandr Sukhanov
 License: MIT
 Core system
 Система мониторинга серверов
-Версия: 8.58.22
+Версия: 8.58.23
 Автор: Александр Суханов (c)
 Лицензия: MIT
 Ядро системы
