5d19585197d374d1e98922fa6b3a78342b1be53b
Alexandr <aleksandr.i.sukhanov@gmail.com>
2026-05-11 03:00:30 +0700

    Обновить TLS-инструкции и повысить версию до 8.58.34

--- extensions/__init__.py
+++ extensions/__init__.py
@@ -1,11 +1,11 @@
 """
 /extensions/__init__.py
-Server Monitoring System v8.58.33
+Server Monitoring System v8.58.34
 Copyright (c) 2025 Aleksandr Sukhanov
 License: MIT
 Extensions interface
 Система мониторинга серверов
-Версия: 8.58.33
+Версия: 8.58.34
 Автор: Александр Суханов (c)
 Лицензия: MIT
 Интерфейс расширений
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """
 /main.py
-Server Monitoring System v8.58.33
+Server Monitoring System v8.58.34
 Copyright (c) 2025 Aleksandr Sukhanov
 License: MIT
 Main launch module
 Система мониторинга серверов
-Версия: 8.58.33
+Версия: 8.58.34
 Автор: Александр Суханов (c)
 Лицензия: MIT
 Основной модуль запуска
